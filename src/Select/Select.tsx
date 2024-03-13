3144bf19d94341b9e9b0d8d02fc63c0b70de32c0
Phantom <phanton7487@gmail.com>
2024-03-13 23:08:51 +0200

    Select.tsx

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, {useState,KeyboardEvent } from "react";
+import React, {useState, KeyboardEvent, useEffect} from "react";
 import styles from './Select.module.css'
 
 export type ItemType = {
@@ -14,9 +14,13 @@ export type SelectPropsType = {
 
 export const Select = (props: SelectPropsType) => {
     const [edit, setEdit] = useState(false)
-    const [hoverElement, setHoverElement] = useState(props.value)
+    const [hoverElement, setHoverElement] = useState<number>(props.value!)
     const selectedItem = props.items.find(i => i.value === props.value)
-    const hoverItem = props.items.find(i => i.value === hoverElement)
+   /* const hoverItem = props.items.find(i => i.value === hoverElement)*/
+
+    useEffect(() => {
+        setHoverElement(props.value!)
+    }, [props.value]);
     const setEditHandler = () => {
         setEdit(!edit)
     }
@@ -24,33 +28,45 @@ export const Select = (props: SelectPropsType) => {
         props.onChange(value)
         setEditHandler()
     }
-    const onKeyUp = (e:KeyboardEvent<HTMLDivElement>) => {
-        for (let i = 0; i < props.items.length; i++){
-            if (props.items[i].value === hoverElement) {
-                setHoverElement(props.items[i+1].value)
-                break
-            }
+    const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoverElement) {
+                    const prevValue = e.key === 'ArrowDown'
+                        ? props.items[i + 1]
+                        : props.items[i - 1]
+                    if (prevValue) {
+                        props.onChange(prevValue.value)
+                        break
+                    }if(!selectedItem){
+                        props.onChange(props.items[0].value)
+                    }
+                }}}
+        if (e.key === 'Enter' || e.key === 'Escape'){
+            setEdit(true)
         }
     }
-    return (
-        <><div className={styles.select} onClick={setEditHandler}
-               onKeyUp={onKeyUp} tabIndex={0}
-        >
 
-                    <span className={styles.main}>
+    return (
+        <>
+            <div className={styles.select} onClick={setEditHandler}
+                 onKeyUp={onKeyUp} tabIndex={0}
+            >
+            <span className={styles.main}>
                         {selectedItem && selectedItem.title}
                     </span>
-            {!edit && <div className={styles.items}>
-                {props.items.map(i =>
-                    <div className={`${styles.item} ${hoverItem === i ? styles.selected : ''}`} key={i.value}
-
-                         onMouseEnter={() => {
-                             setHoverElement(i.value)}}
-                         onClick={() => onItemClick(i.value)}
-                    >{i.title}</div>)}
+                {!edit && <div className={styles.items}>
+                    {
+                        props.items.map(i =>
+                            <div className={`${styles.item} ${hoverElement === i.value ? styles.selected : ''}`} key={i.value}
+                                 onMouseEnter={() => {
+                                     setHoverElement(i.value)
+                                 }}
+                                 onClick={() => onItemClick(i.value)}
+                            >{i.title}</div>)}
+                </div>
+                }
             </div>
-            }
-        </div>
         </>
     )
 }
