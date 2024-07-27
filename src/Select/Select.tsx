6c0a4fcdf71abcc0eb6c8f439b04dd2e1aacb23b
Phantom <phanton7487@gmail.com>
2024-07-27 18:56:55 +0300

    AnalogsClockView.tsx

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,5 +1,6 @@
 import React, {useState, KeyboardEvent, useEffect} from "react";
 import styles from './Select.module.css'
+import {number} from "prop-types";
 
 export type ItemType = {
     title: string
@@ -15,17 +16,21 @@ onChange: (value: any) => void
 export const Select = (props: SelectPropsType) => {
     const selectedItem = props.items.find(i => i.value === props.value);
     const [edit, setEdit] = useState(false)
+    const [hoverElement, sethoverElement] = useState(props.value)
+
     const setEditHandler = () => {
         setEdit(!edit)
     }
     return (
         <>
             <div className={styles.select} onClick={setEditHandler}>
-           <span>{selectedItem?.title}</span>
-                {!edit &&     <div className={styles.items}>
-                    {props.items.map(i =>
-                        <div key={i.value} onClick={()=> {props.onChange(i.value)}}>{i.title}</div>
-                    )}
+           <span className={styles.main}>{selectedItem?.title}</span>
+                {!edit &&  <div className={styles.items} >
+                    {props.items.map(i=>
+                        <div className={`${styles.item} ${hoverElement === i.value ? styles.selected : ''}`}
+                            onClick={()=>props.onChange(i.value)}
+                             onMouseEnter={(e)=>sethoverElement(i.value)}
+                    >{i.title}</div>)}
                 </div>}
 
             </div>
